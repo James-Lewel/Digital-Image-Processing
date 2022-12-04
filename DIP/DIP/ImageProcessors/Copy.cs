5daff086d64bc4ca174185ed49c9a893e63ee922
James Lewel Padecio <jameslewel.padecio@cit.edu>
2022-12-04 23:58:53 +0800

    Add & implement copy class

---
+++ DIP/DIP/ImageProcessors/Copy.cs
@@ -0,0 +1,16 @@
+﻿using DIP.Processors;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DIP.ImageProcessors
+{
+    internal class Copy
+    {
+        private static readonly Processor _processor = new Processor();
+
+        public static Bitmap Process(Bitmap originalBitap)
+        {
+            return _processor.CopyImage(originalBitap);
+        }
+    }
+}
